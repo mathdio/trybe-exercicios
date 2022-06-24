41c5d2057b36ec4e2c118c9d2d627aca3fa82ad6
matheus <mathdiogenes@gmail.com>
2022-06-24 15:12:30 -0300

    prática do dia 4.2

--- exercises/dia-4.2-introducao-js-e-logica/script.js
+++ exercises/dia-4.2-introducao-js-e-logica/script.js
@@ -128,3 +128,34 @@ for (let index = 1; index < numbers.length; index += 1) {
 }
 
 console.log(numbers);
+
+// 2 - Ordene o array numbers em ordem decrescente e imprima seus valores;
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let index = 1; index < numbers.length; index += 1) {
+  for (let secondIndex = 0; secondIndex < numbers.length; secondIndex += 1) {
+    if (numbers[secondIndex] < numbers[index]) {
+      let position = numbers[secondIndex];
+      numbers[secondIndex] = numbers[index];
+      numbers[index] = position;
+    }
+  }
+}
+
+console.log(numbers);
+
+// 3 - Agora crie um novo array a partir do array numbers, sem perdê-lo. Cada valor do novo array deverá ser igual ao valor correspondente no array numbers multiplicado pelo seguinte. Por exemplo: o primeiro valor do novo array deverá ser 45, pois é a multiplicação de 5 (primeiro valor) e 9 (valor seguinte). Já o segundo valor do novo array deverá ser 27, pois é a multiplicação de 9 (segundo valor) e 3 (valor seguinte), e assim por diante. Caso não haja próximo valor, a multiplicação deverá ser feita por 2. Faça isso utilizando o for e o método push. O resultado deve ser o array abaixo: [45, 27, 57, 1330, 560, 800, 200, 70, 945, 54]
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let newNumbers = [];
+
+for (let i = 0; i < numbers.length; i += 1) {
+  if (i < numbers.length - 1) {
+    newNumbers.push(numbers[i] * numbers[i + 1]);
+  } else {
+    newNumbers.push(numbers[i] * 2);
+  }
+}
+
+console.log(newNumbers);
