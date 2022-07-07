3d43898e0d3c3c8ebf984c1dce824d74a706dd94
matheus <mathdiogenes@gmail.com>
2022-07-06 21:57:25 -0300

    finalização prática dia 6.1

--- m1fundamentos/b6-html-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/pratica.js
+++ m1fundamentos/b6-html-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/pratica.js
@@ -4,19 +4,13 @@ let validation = document.getElementsByClassName('validation');
 
 
 
-submitButton.addEventListener('click', function (e) {
-  // if (photos.checked === false) {
-  //   e.preventDefault();
-  //   alert('Concorde com o envio de fotos.');
-  // } else {
-  //   alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.');
-  // };
-
-  for (let i = 0; i < validation.length; i += 1) {
-    if (validation[i].value === '') {
-      e.preventDefault();
-      alert('Dados inválidos');
-      break;
-    };
-  };
-});
+// submitButton.addEventListener('click', function (e) {
+//   for (let i = 0; i < validation.length; i += 1) {
+//     if (validation[i].value === '') {
+//       e.preventDefault();
+//       alert('Dados inválidos');
+//       break;
+//     };
+//   };
+// });
+// Essa validação não parece ser necessária, sendo possível nesse caso usar validação configurando o required no forms.
