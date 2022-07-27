f8a896b97a8228a01bd39df033e9f0a657c1adda
matheus <mathdiogenes@gmail.com>
2022-07-27 20:50:13 -0300

    prática do dia 8.5

---
+++ m1fundamentos/b8-higher-order-functions-JS-ES6/dia-5-spread-operator-rest-parameter-destructuring-object-property-shorthand-default-parameters/pratica/exercise2.js
@@ -0,0 +1,9 @@
+// 2 - Crie uma função sum que dado um número variável de elementos retorna a soma desses elementos.
+// Dica: use parâmetro rest.
+
+
+function sum(...numbers) {
+  return numbers.reduce((acc, curr) => acc + curr);
+};
+
+console.log(sum(1, 2, 3, 4, 5));
